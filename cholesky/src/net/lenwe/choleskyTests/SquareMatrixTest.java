9a7af8ec0eebf4144fdee4521d9b1d06623e0c65
Jakub Kramarz <lenwe@lenwe.net>
2013-01-07 00:07:32 +0100

    Typing mistake in tests

--- cholesky/src/net/lenwe/choleskyTests/SquareMatrixTest.java
+++ cholesky/src/net/lenwe/choleskyTests/SquareMatrixTest.java
@@ -15,34 +15,47 @@ public class SquareMatrixTest {
 		SquareMatrix s = new SquareMatrix(d);
 		SquareMatrix t = new SquareMatrix(s);
 		assertTrue(s.eq(t));
+		assertTrue(s.dim() == t.dim());
 		t.set(1, 1, new Complex(1,1));
 		assertFalse(s.eq(t));
 	}
 
 	@Test
 	public void testIsSymmetric1() throws Exception {
-		double d[] = { 1, 2, 2, 0 };
+		double d[] = {
+			1, 2,
+			2, 0
+		};
 		SquareMatrix s = new SquareMatrix(d);
 		assertTrue(s.isSymmetric());
 	}
 	
 	@Test
 	public void testIsSymmetric2() throws Exception {
-		double d[] = { 1, 2, 1, 2 };
+		double d[] = {
+			1, 2,
+			1, 2
+		};
 		SquareMatrix s = new SquareMatrix(d);
 		assertFalse(s.isSymmetric());
 	}
 
 	@Test
 	public void testIsHermitian1() throws Exception {
-		Complex c[] = { new Complex(1, -1), new Complex(2, 1), new Complex(2, 1), new Complex(0, -1) };
+		Complex c[] = {
+			new Complex(1, -1), new Complex(2, 1),
+			new Complex(2, -1), new Complex(0, -1)
+		};
 		SquareMatrix s = new SquareMatrix(c);
 		assertTrue(s.isHermitian());
 	}
 
 	@Test
 	public void testIsHermitian2() throws Exception {
-		Complex c[] = { new Complex(1, -1), new Complex(2, -1), new Complex(2, 1), new Complex(0, -1) };
+		Complex c[] = {
+			new Complex(1, -1), new Complex(2, -1),
+			new Complex(2, -1), new Complex(0, -1)
+		};
 		SquareMatrix s = new SquareMatrix(c);
 		assertFalse(s.isHermitian());
 	}
